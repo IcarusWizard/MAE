2d14ada17f2b9449176509e1a57a0417bb2c3f46
Icarus <wizardicarus@gmail.com>
2021-11-27 23:51:06 +0800

    update classifer training

--- train_classifier.py
+++ train_classifier.py
@@ -1,34 +1,46 @@
+import os
 import argparse
 import math
 import torch
 import torchvision
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import ToTensor, Compose, Normalize
 from tqdm import tqdm
 
 from model import *
+from utils import setup_seed
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--batch_size', type=int, default=1024)
+    parser.add_argument('--max_device_batch_size', type=int, default=256)
     parser.add_argument('--base_learning_rate', type=float, default=1e-3)
     parser.add_argument('--weight_decay', type=float, default=0.05)
     parser.add_argument('--total_epoch', type=int, default=100)
     parser.add_argument('--warmup_epoch', type=int, default=5)
-    parser.add_argument('--pretrained_model_path', type=str, default='vit-t-mae.pt')
-    parser.add_argument('--output_model_path', type=str, default='vit-t-mae-finetuned.pt')
+    parser.add_argument('--pretrained_model_path', type=str, default=None)
+    parser.add_argument('--output_model_path', type=str, default='vit-t-mae-scratch.pt')
 
     args = parser.parse_args()
 
+    setup_seed(args.seed)
+
+    assert args.batch_size % args.max_device_batch_size == 0
+    steps_per_update = args.batch_size // args.max_device_batch_size
+
     train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
     val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-    train_dataloader = torch.utils.data.DataLoader(train_dataset, args.batch_size, shuffle=True, num_workers=4)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, args.batch_size, shuffle=False, num_workers=4)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, args.max_device_batch_size, shuffle=True, num_workers=4)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, args.max_device_batch_size, shuffle=False, num_workers=4)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     if args.pretrained_model_path is not None:
         model = torch.load(args.pretrained_model_path, map_location='cpu')
+        writer = SummaryWriter(os.path.join('logs', 'cifar10', 'pretrain-cls'))
     else:
         model = MAE_ViT()
+        writer = SummaryWriter(os.path.join('logs', 'cifar10', 'scratch-cls'))
     model = ViT_Classifier(model.encoder, num_classes=10).to(device)
 
     loss_fn = torch.nn.CrossEntropyLoss()
@@ -39,25 +51,29 @@ if __name__ == '__main__':
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func, verbose=True)
 
     best_val_acc = 0
+    step_count = 0
+    optim.zero_grad()
     for e in range(args.total_epoch):
         model.train()
         losses = []
         acces = []
         for img, label in tqdm(iter(train_dataloader)):
+            step_count += 1
             img = img.to(device)
             label = label.to(device)
             logits = model(img)
             loss = loss_fn(logits, label)
             acc = acc_fn(logits, label)
-            optim.zero_grad()
             loss.backward()
-            optim.step()
+            if step_count % steps_per_update == 0:
+                optim.step()
+                optim.zero_grad()
             losses.append(loss.item())
             acces.append(acc.item())
         lr_scheduler.step()
-        avg_loss = sum(losses) / len(losses)
-        avg_acc = sum(acces) / len(acces)
-        print(f'In epoch {e}, average training loss is {avg_loss}, average training acc is {avg_acc}.')
+        avg_train_loss = sum(losses) / len(losses)
+        avg_train_acc = sum(acces) / len(acces)
+        print(f'In epoch {e}, average training loss is {avg_train_loss}, average training acc is {avg_train_acc}.')
 
         model.eval()
         with torch.no_grad():
@@ -71,11 +87,14 @@ if __name__ == '__main__':
                 acc = acc_fn(logits, label)
                 losses.append(loss.item())
                 acces.append(acc.item())
-            avg_loss = sum(losses) / len(losses)
-            avg_acc = sum(acces) / len(acces)
-            print(f'In epoch {e}, average validation loss is {avg_loss}, average validation acc is {avg_acc}.')  
+            avg_val_loss = sum(losses) / len(losses)
+            avg_val_acc = sum(acces) / len(acces)
+            print(f'In epoch {e}, average validation loss is {avg_val_loss}, average validation acc is {avg_val_acc}.')  
 
-        if avg_acc > best_val_acc:
-            best_val_acc = avg_acc
+        if avg_val_acc > best_val_acc:
+            best_val_acc = avg_val_acc
             print(f'saving best model with acc {best_val_acc} at {e} epoch!')       
             torch.save(model, args.output_model_path)
+
+        writer.add_scalars('cls/loss', {'train' : avg_train_loss, 'val' : avg_val_loss}, global_step=e)
+        writer.add_scalars('cls/acc', {'train' : avg_train_acc, 'val' : avg_val_acc}, global_step=e)

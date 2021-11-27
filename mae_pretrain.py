b8c3437d725b073c80eabe0457b985002a59e052
Icarus <wizardicarus@gmail.com>
2021-11-27 15:11:39 +0800

    fix typo

--- mae_pretrain.py
+++ mae_pretrain.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
         with torch.no_grad():
             val_img = torch.stack([val_dataset[i][0] for i in range(16)])
             val_img = val_img.to(device)
-            predicted_val_img, mask = model(img)
+            predicted_val_img, mask = model(val_img)
             predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
             img = torch.cat([val_img * (1 - mask), predicted_val_img, val_img], dim=0)
             img = rearrange(img, '(v h1 w1) c h w -> c (h1 h) (w1 v w)', w1=2, v=3)
